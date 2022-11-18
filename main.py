5705c63598b050d727118291d09f8a1ca83c46c2
thondascully <thondascully@berkeley.edu>
2022-11-17 17:27:55 -0800

    Replace main2.py with main.py

--- main.py
+++ main.py
@@ -1,29 +1,75 @@
-import random
-import math
+import threading
+
+current_arr_size = 100
+thread_count = 4
 
 words = []
 
-with open("wordlist.txt") as file:
-    for line in file.readlines():
-        words.append(line)
+char_to_int = {'a': 0, 'b': 1, 'c': 2, 'd': 3,
+'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 
+'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13,
+'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18,
+'t': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 
+'y': 24, 'z': 25, 'I': 26, '\n': 0}
 
 char_to_int = {'a': '0', 'b': '1', 'c': '2', 'd': '3',
 'e': '4', 'f': '5', 'g': '6', 'h': '7', 'i': '8', 
 'j': '9', 'k': '10', 'l': '11', 'm': '12', 'n': '13',
 'o': '14', 'p': '15', 'q': '16', 'r': '17', 's': '18',
 't': '19', 'u': '20', 'v': '21', 'w': '22', 'x': '23', 
-'y': '24', 'z': '25', 'I': '26'}
+'y': '24', 'z': '25', 'I': '26', '\n': 0}
 
-def conv_str_to_int(word):
-    word = word.replace("\n", "")
-    word_ = ""
-    for letter in word:
-        word_ += char_to_int[letter]
-    return word_
+with open("wordlist.txt") as file:
+    words = file.readlines()[:current_arr_size]
 
-ord_arr = [conv_str_to_int(words[i]) for i in range(len(words))]
+def h_(a, b, raw) -> int:
+    h = a
+    for c in raw:
+        h = ((h << b) + h) + ord(c)
+    return int(h) % current_arr_size
 
-def hash(word):
-    a = 0
-    b = 0
-    pass
+def h2(a, b, raw) -> int:
+    c = '0'
+    for i in raw:
+        c += char_to_int[i] + a
+        c %= b
+    return c % current_arr_size
+
+#sort()
+
+# h(k) = ((ak + b)%p)%m
+
+# Can you judge future parameters on past performance?
+best_of_the_best = []
+def gen_parameters(word_list, thread_num):
+    start = (current_arr_size // thread_count) * (thread_num - 1)
+    end = (current_arr_size // thread_count) * (thread_num)
+    count = current_arr_size
+
+    for a in range(start, end):
+        for b in range(1, current_arr_size):
+            frequency: list = [0 for x in range(current_arr_size)]
+            for i in words:
+                frequency[h_(a, b, i)] += 1
+            collisions = len(list(filter(lambda x: not x, frequency)))
+            if count > collisions:
+                count = collisions
+                best = [a, b]
+    best_of_the_best.append([best, count])
+
+threads = []
+for x in range(thread_count): 
+    t = threading.Thread(target=gen_parameters, args=(words, x + 1))
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
+
+print(best_of_the_best)
+
+def list_sort(bests):
+    count = 0
+    for i in bests:
+        count += (bests[i][0] + bests[i][1])
+    
