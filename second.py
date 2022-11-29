c02d8bd16a9476fd95147e9cb94a43317527c85b
thondascully <thondascully@berkeley.edu>
2022-11-29 01:09:08 -0800

    Successful with m=30000, size=15

--- second.py
+++ second.py
@@ -1,24 +1,22 @@
-current_arr_size = 10
-test = ["hello", "there", "my", "name", "is", "teo", "honda", "scully"]
+current_arr_size = 15
 
 words = []
 
 with open("wordlist.txt") as file:
-    #words = file.readlines()[:current_arr_size]
-    pass
+    words = file.readlines()[:current_arr_size]
 
 def hash_(word, m):
     c = ''.join(format(ord(i),'b').zfill(8) for i in word)
     d = (int(c) * m) >> (len(c) - 4)
-    return d % len(test)
+    return d % len(words)
     
-for m in range(10000):
+for m in range(30000):
     d = {}
-    for word in test:
+    for word in words:
         c = hash_(word, m)
         if c in d:
             d[c] += 1
         else:
             d[c] = 1
-    if len(d.keys()) == len(test):
+    if len(d.keys()) == len(words):
         print(f'{d} | {m}')
