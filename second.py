1f12d616a24f759837cdf277e14b19b23ab136af
thondascully <thondascully@berkeley.edu>
2022-11-29 01:06:34 -0800

    Successfully generate m

--- second.py
+++ second.py
@@ -7,13 +7,18 @@ with open("wordlist.txt") as file:
     #words = file.readlines()[:current_arr_size]
     pass
 
-m = 31
-def hash(word):
+def hash_(word, m):
     c = ''.join(format(ord(i),'b').zfill(8) for i in word)
     d = (int(c) * m) >> (len(c) - 4)
     return d % len(test)
     
-for word in test:
-    print(hash(word))
-    for m in range(10000):
-        
+for m in range(10000):
+    d = {}
+    for word in test:
+        c = hash_(word, m)
+        if c in d:
+            d[c] += 1
+        else:
+            d[c] = 1
+    if len(d.keys()) == len(test):
+        print(f'{d} | {m}')
